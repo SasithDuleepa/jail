84dce5318376eeaf0e24d41c51cd48c411831a26
SasithDuleepa <duleepajayathissa@gmail.com>
2023-09-14 09:16:32 +0530

    nav link

--- src/components/navBar/navbar.jsx
+++ src/components/navBar/navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
             <p className='navbar-div-div1-logo'>Logo</p>
         </div>
         <div className='navbar-div-div2'>
-            <a href='#' className='navbar-div-div2-a-home' >Home</a>
+            <a href='/' className='navbar-div-div2-a-home' >Home</a>
             <a href='#' className='navbar-div-div2-a-Catergories' >Catergories</a>
             <a href='#' className='navbar-div-div2-a-Privious_Orders' >Privious Orders</a>
         </div>
